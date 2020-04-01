185a6fc297ca7d814ada7161d92e1eca0ba32386
Senik <senik@Seniks-MBP.lan>
2020-04-01 16:48:22 -0500

    submission 1.0

--- js/app.js
+++ js/app.js
@@ -12,46 +12,43 @@
  * JS Standard: ESlint
  * 
 */
+(function () {
+    // Global variables
+    const navbar = document.getElementById('navbar__list');
 
-/**
- * Define Global Variables
- * 
-*/
+    // For loop constructor
+    for (let i = 1; i < 7; i++) {
+        // Create Section Element
+        const textToAdd =
+            `<div class='landing__container'>
+            <h2>Section ${i}</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
+            <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+            </div>`;
+        const sectionID = document.getElementById('s' + i);
+        sectionID.insertAdjacentHTML('beforeend', textToAdd);
 
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
+        // Create Li elements, onclick jump to section
+        const liElements = `<li><a href="#s${i}">Section ${i}</a></li>`;
+        navbar.insertAdjacentHTML('beforeend', liElements);
+    }
 
 
-// Add class 'active' to section when near top of viewport
+    function onScroll(event) {
+        var sections = document.querySelectorAll('li a');
+        var scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
 
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+        for (var i = 0; i < sections.length; i++) {
+            var currLink = sections[i];
+            var val = currLink.getAttribute('href');
+            var refElement = document.querySelector(val);
+            if (refElement.offsetTop <= scrollPos && (refElement.offsetTop + refElement.offsetHeight > scrollPos)) {
+                document.querySelector('li a').classList.remove('active');
+                currLink.classList.add('active');
+            } else {
+                currLink.classList.remove('active');
+            }
+        }
+    };
+    window.document.addEventListener('scroll', onScroll);
+})();
